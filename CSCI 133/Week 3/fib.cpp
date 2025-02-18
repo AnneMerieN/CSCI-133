1bc7256aa5e0cda7f23093262bd4eab5e3276f9d
Anne Merie Nguyen <annemerie001@gmail.com>
2025-02-18 09:24:52 -0800

    Implemented Recursive Fibonacci

---
+++ CSCI 133/Week 3/fib.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+
+int fib (int n) {
+    if (n == 0) { // Base Case: Fib of 0 is 0
+        return 0;
+    } else if (n == 1) { // Base Case: Fib of 1 is 1
+        return 1;
+    } else { // Each subsequent number is the sum of the two preceding numbers 
+        return fib(n - 1) + fib(n-2); // Recursive case
+    }
+}
+
+
+int main() {
+
+
+
+    return 0;
+}
